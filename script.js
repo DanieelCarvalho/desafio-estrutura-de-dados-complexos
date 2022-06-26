050c8877df0abd2dadf73d5fb53c1c97a3cbabcd
Danieel Carvalho <Danielscg2012@gmail.com>
2022-06-26 11:42:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let lista= ["maça", "banana", "abacate", "morango"];
+console.log(lista)
+
+lista.unshift("uva")
+console.log(lista)
+
+lista.pop()
+console.log(lista)
+
+lista.push("laranja", "melancia" )
+console.log(lista)
+
+lista.shift()
+console.log(lista)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
+
